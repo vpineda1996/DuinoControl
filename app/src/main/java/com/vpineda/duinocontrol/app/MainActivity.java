0287d24c1be6eba0c11e3ebe8a53e8f06492b9dd
vpineda1996 <vpineda1996@gmail.com>
2015-01-07 23:04:40 -0800

    Refactored to make a Server Fragment (easier to read)

--- app/src/main/java/com/vpineda/duinocontrol/app/MainActivity.java
+++ app/src/main/java/com/vpineda/duinocontrol/app/MainActivity.java
@@ -1,23 +1,29 @@
 package com.vpineda.duinocontrol.app;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.vpineda.duinocontrol.app.settings.MainPreferences;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+    // =====================================
+    // OVERWRITES
+    // =====================================
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        PreferenceManager.setDefaultValues(this,R.xml.preferences,false);
         setContentView(R.layout.activity_main);
 
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+
 
 
     }
@@ -27,8 +33,9 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -46,4 +53,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
